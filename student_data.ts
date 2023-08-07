e4cfc67a8c467514c5a1eab8e3452fbbc39bdd02
Malik Abid <malikabidzz0306@gmail.com>
2023-08-07 17:15:14 +0500

    result-check

---
+++ student_data.ts
@@ -0,0 +1,56 @@
+let studens = [
+  {
+    rollNo: 1,
+    name: "Abid",
+    class: "Inter-Part1",
+    subjects: { urdu: 41, English: 38, Ist: 31, Ps: 51 },
+  },  {
+    rollNo: 2,
+    name: "Abu Bakar",
+    class: "Matric-Part1",
+    subjects: { urdu: 43, English: 24, Ist: 34, Ps: 51 },
+  },  {
+    rollNo: 3,
+    name: "Asad",
+    class: "Inter-Part2",
+    subjects: { urdu: 12, English: 22, Ist: 54, Ps: 32 },
+  },  {
+    rollNo: 4,
+    name: "Danish",
+    class: "Matric-Part2",
+    subjects: { urdu: 41, English: 26, Ist: 45, Ps: 43 },
+  },  {
+    rollNo: 5,
+    name: "Numan",
+    class: "Matric-Part1",
+    subjects: { urdu: 25, English: 29, Ist: 30, Ps: 31 },
+  },  {
+    rollNo: 6,
+    name: "Mubashir",
+    class: "Inter-Part1",
+    subjects: { urdu: 34, English: 32, Ist: 34, Ps: 50 },
+  },  {
+    rollNo: 7,
+    name: "Mudasar",
+    class: "matric",
+    subjects: { urdu: 56, English: 23, Ist: 32, Ps: 45 },
+  },  {
+    rollNo: 8,
+    name: "Ali",
+    class: "Matric-Part1",
+    subjects: { urdu: 40, English: 29, Ist: 45, Ps: 20 },
+  },  {
+    rollNo: 9,
+    name: "Usman",
+    class: "Matric-Part2",
+    subjects: { urdu: 24, English: 49, Ist: 36, Ps: 40 },
+  },
+  {
+    rollNo: 10,
+    name: "Ahmad",
+    class: "Inter-Part1",
+    subjects: { urdu: 43, English: 54, Ist: 35, Ps: 50 },
+  },
+];
+
+
